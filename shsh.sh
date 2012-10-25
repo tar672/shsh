747a7166bf4d949a43e2b62e24193b02394033e5
tom <tomasrobertson@ymail.com>
2012-10-25 14:11:36 +0100

    working for create

--- shsh.sh
+++ shsh.sh
@@ -1,18 +1,30 @@
 #!/bin/sh
 shsh(){
-        while getopts ":r" opt; do
-         if [ "$opt" -eq "r" ]; then
-            sed -e "/alias $1/d" ~/.aliases >~/.aliases.tmp;
-            return
-         fi
-        done
-        touch ~/.aliases.tmp;
-        touch ~/.aliases;
-        sed -e "/alias $1/d" ~/.aliases >~/.aliases.tmp;
-        cat ~/.aliases.tmp > ~/.aliases;
-        STRING="alias $1='cd `pwd`'";
-        eval $STRING;
-        echo $STRING >> ~/.aliases;
-        rm ~/.aliases.tmp;
+	local removeflag=0;
+	while getopts r: opt
+	do
+		echo "$opt and $OPTARG"
+		if [ "$opt" = "r" ];
+		then
+			sed -e "/alias $OPTARG/d" ~/.aliases > ~/.aliases.tmp;
+			unalias $OPTARG;
+			echo "in loop";
+		fi
+		removeflag=1;
+	done
+	if [ "$removeflag" -eq "0" ];
+	then
+		echo "out loop";
+		TEMPALIASES=`mktemp`
+		touch ~/.aliases;
+		sed -e "/alias $1/d" ~/.aliases >$TEMPALIASES;
+		cat $TEMPALIASES > ~/.aliases;
+		STRING="alias $1='cd `pwd`'";
+		eval $STRING;
+		echo $STRING >> ~/.aliases;
+		rm -f $TEMPALIASES;
+	else
+		shift $OPTIND-1;	
+	fi
 }
 touch ~/.aliases
